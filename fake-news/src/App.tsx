5b66ee7b0c42e0fc3b1ec4d4586cdd084f202414
Henrique Eduardo <henrique.eduardo.abrantes.moura@gmail.com>
2026-04-14 20:23:07 -0300

    react ts

---
+++ fake-news/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+      </section>
+      <div><h1>Hello World</h1></div>
+
+    </>
+  )
+}
+
+export default App
